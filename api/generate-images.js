af12df85f1948e5c262cbd56228f56a4f9571f45
sytnikov <sytnikov.alexey@gmail.com>
2024-08-06 00:45:46 +0300

    cors for all chrome extensions

--- api/generate-images.js
+++ api/generate-images.js
@@ -9,12 +9,16 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 const corsOptions = {
-  origin: [
-    'chrome-extension://mogkblnojamgejdhgffbelpikocalapb',
-    'chrome-extension://cabgmgcmnkeopfenfjmpgbalmgchajij',
-  ],
+  origin: (origin, callback) => {
+    if (origin && origin.startsWith('chrome-extension://')) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  allowedHeaders: ['Content-Type', 'Authorization'],
+  allowedHeaders: 'Content-Type,Authorization',
+  credentials: true,
 }
 
 app.use(cors(corsOptions))
