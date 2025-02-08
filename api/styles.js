d0efedf9485197c978205f1833c2f627c044ed8a
sytnikov <sytnikov.alexey@gmail.com>
2025-02-08 16:11:16 +0200

    added styles file  and positive and nagative prompts with weights

---
+++ api/styles.js
@@ -0,0 +1,8 @@
+export const styles = {
+  stencil: {
+    positive:
+      'stencil style, high-contrast black and white, bold solid shapes, clean cutout, sharp edges, simple silhouette, no gradients, no shading, clear and minimalistic, strong graphic impact, no tiny details, no background clutter, cohesive structure, smooth contours',
+    negative:
+      'grayscale, shading, gradients, floating or fragmented black elements, blurred edges, excessive details, noise, cluttered background, soft or fuzzy lines, overcomplicated design, unnecessary complexity, disconnected parts, distortion, low contrast, smudging, artifacts, asymmetry, missing facial features, unwanted text',
+  },
+}
